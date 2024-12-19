1f0b330344e1967f310dc4cca6decf73e511e571
mar-ina-17 <marina.workplace@gmail.com>
2024-12-19 14:13:21 +0200

    server post logic chamged

--- api/server.js
+++ api/server.js
@@ -1,31 +1,43 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+const jsonServer = require("json-server");
+const fs = require("fs");
+const path = require("path");
 
-const server = jsonServer.create()
+const server = jsonServer.create();
+const filePath = path.join("db.json");
+const middlewares = jsonServer.defaults();
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+// Load the current database
+let db = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+// Middleware to replace the /people data
+server.post("/people", (req, res) => {
+  try {
+    // Overwrite the people array with new data
+    db.people = req.body;
+
+    // Save the updated database back to the file
+    fs.writeFileSync(filePath, JSON.stringify(db, null, 2));
+
+    res.status(200).send("People data replaced successfully");
+  } catch (err) {
+    console.error("Error updating people:", err);
+    res.status(500).send("Failed to update people data");
+  }
+});
 
 // Comment out to allow write operations
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(db);
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
